728405aec78ea65bd2372ddbd0f741e8fcbd5f6a
Poojan Patel <poojan.patel1126@gmail.com>
2022-04-01 09:15:34 -0700

    UI added

---
+++ FacialRecognitionApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  FacialRecognitionApp
+//
+//  Created by Poojan on 4/1/22.
+//
+
+import UIKit
+import Vision
+import Photos
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
